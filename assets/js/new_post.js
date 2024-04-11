29d1742cc71bcf36f811b532a7887ba806bf419f
Pedro <113944862+callme-ph@users.noreply.github.com>
2024-04-11 00:24:22 -0300

    Adicionado a rotina que possibilita posts
    Adicionado a rotina que possibilita posts na página de postagens

--- assets/js/new_post.js
+++ assets/js/new_post.js
@@ -1,7 +1,30 @@
-function elementFromHtml(html) {
-    const template = document.createElement("template");
+document.addEventListener('DOMContentLoaded', function () { 
+    const createPostBtn = document.getElementById('#post_button');
+    const postContainer = document.querySelector('.content'); 
+    const postForm = document.getElementById('newpost_form'); 
+    
+    postForm.addEventListener('submit', function (event) {
+		event.preventDefault(); 
 
-    template.inneHTML = html.trim();
+        const postTitle = document.getElementById('post_title').value; 
+        const postDescription = document.getElementById('post_text').value;
 
-    return template.content.firstElementChild;
-}
+    //alert("Entrou na função submit");
+
+    const newPost = document.createElement('div'); 
+		newPost.className = 'new_post'; 
+		newPost.innerHTML = `
+        <h1 class="post-title"> ${postTitle}</h1><br>
+
+        <p class="post-description">
+        ${postDescription.substring(0, 100)}...</p>`;
+
+        //alert('Passou do new post');
+
+        postContainer.insertBefore(newPost, postContainer.firstChild); 
+
+		// Reset the form 
+		postForm.reset(); 
+        //alert('Form reset');
+    })
+})
